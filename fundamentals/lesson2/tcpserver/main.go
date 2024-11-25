071c1e372be9eebae3d87d76a36edf8a33e26f94
anishsharma21 <anisharma2103@gmail.com>
2024-11-25 22:10:28 +1100

    Course - Lesson 2: TCP Clients - first part is setting up the TCP server, which I did without any help this time round, got it working, now need to write the TCP clients to connect to it.

---
+++ fundamentals/lesson2/tcpserver/main.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"net"
+	"os"
+	"strconv"
+	"strings"
+)
+
+var serverErrCount int = 0
+
+func main() {
+	var port uint16 = 8080
+	if len(os.Args) > 2 {
+		portInt, err := strconv.ParseInt(os.Args[1], 10, 16)
+		if err != nil {
+			fmt.Printf("Error parsing %q: %e\n", os.Args[1], err)
+			return
+		}
+		port = uint16(portInt)
+	}
+	tcpserver(port)
+}
+
+func tcpserver(port uint16) {
+	listener, err := net.Listen("tcp", fmt.Sprintf(":%d", port))
+	if err != nil {
+		fmt.Println("Error starting TCP server:", err)
+		return
+	}
+	defer listener.Close()
+
+	fmt.Printf("TCP Server started on port %d...\n", port)
+
+	for {
+		if serverErrCount > 3 {
+			fmt.Println("Too many server errors... shutting down.")
+			return
+		}
+		conn, err := listener.Accept()
+		if err != nil {
+			fmt.Println("Error connecting to client:", err)
+			serverErrCount++
+			continue
+		}
+		fmt.Println("Connected to client:", conn.RemoteAddr().String())
+		_, err = conn.Write([]byte("\nWELCOME\n>> "))
+		if err != nil {
+			fmt.Printf("Error sending welcome message to %s: %e\n", conn.RemoteAddr().String(), err)
+			conn.Close()
+			serverErrCount++
+			continue
+		}
+		go handleClient(conn)
+	}
+}
+
+func handleClient(conn net.Conn) {
+	defer conn.Close()
+	for {
+		message, err := bufio.NewReader(conn).ReadString('\n')
+		if err != nil {
+			fmt.Printf("%s disconnected: %e\n", conn.RemoteAddr().String(), err)
+			return
+		}
+
+		fmt.Printf("Message from %s: %s\n", conn.RemoteAddr().String(), strings.TrimSpace(message))
+
+		_, err = conn.Write([]byte("Echo: "+ strings.TrimSpace(message) + "\n>> "))
+		if err != nil {
+			fmt.Printf("Error sending message to %s: %e\n", conn.RemoteAddr().String(), err)
+			return
+		}
+	}
+}
