142a5b99c2dddd1749fc700a11458389ab77da35
anishsharma21 <anisharma2103@gmail.com>
2024-11-26 13:45:21 +1100

    Course - Lesson 3: TCP Packet Reading - another TCP server, getting more practice, but this time round, reading in packets of a specified size, echoing output as well. Another TCP server with a scanner, interesting use cases all round, next commits will involve TCP clients that message with packets, eventually, setting up a custom protocol with a length-prefixed data strategy.

---
+++ fundamentals/lesson3/tcppacketserver/main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"log"
+	"net"
+)
+
+const defaultPort uint16 = 8080
+
+// TCP server that reads in packets rather than strings
+func main() {
+	listener, err := net.Listen("tcp", fmt.Sprintf(":%d", defaultPort))
+	if err != nil {
+		log.Fatalln("Error starting server:", err)
+	}
+	defer listener.Close()
+
+	fmt.Printf("Starting server on port %d...\n", defaultPort)
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Fatalln("Error accepting connection:", err)
+		}
+		fmt.Println("Connected to client:", conn.RemoteAddr().String())
+		go handleClient(conn)
+	}
+}
+
+func handleClient(conn net.Conn) {
+	defer conn.Close()
+	reader := bufio.NewReader(conn)
+	packet := make([]byte, 1024)
+
+	for {
+		_, err := reader.Read(packet)
+		if err != nil {
+			if err == io.EOF {
+				log.Printf("%s disconnected\n", conn.RemoteAddr().String())
+			} else {
+				log.Printf("Error reading from %s: %e\n", conn.RemoteAddr().String(), err)
+			}
+			return
+		}
+
+		fmt.Printf("Packet from %s: %x\n", conn.RemoteAddr().String(), packet)
+
+		_, err = conn.Write(packet)
+		if err != nil {
+			log.Printf("Error sending packet to %s: %e\n", conn.RemoteAddr().String(), err)
+		}
+	}
+}
---
+++ fundamentals/lesson3/tcpscannerserver/main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"net"
+)
+
+const port uint16 = 8080
+
+func main() {
+	ln, err := net.Listen("tcp", fmt.Sprintf(":%d", port))
+	if err != nil {
+		log.Fatalln("Error starting server:", err)
+	}
+	defer ln.Close()
+
+	log.Printf("Server started on port %d...\n", port)
+
+	for {
+		conn, err := ln.Accept()
+		if err != nil {
+			log.Println("Failed to accept connection:", err)
+			continue
+		}
+		log.Println("Connected to client:", conn.RemoteAddr().String())
+		go handleClient(conn)
+	}
+}
+
+func handleClient(conn net.Conn) {
+	defer conn.Close()
+	scanner := bufio.NewScanner(conn)
+
+	for scanner.Scan() {
+		message := scanner.Text()
+		log.Printf("%s: %s\n", conn.RemoteAddr().String(), message)
+		_, err := conn.Write([]byte(message))
+		if err != nil {
+			log.Printf("Error sending message to %s: %v\n", conn.RemoteAddr().String(), err)
+			return
+		}
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Printf("Error reading message from %s: %v\n", conn.RemoteAddr().String(), err)
+	} else {
+		log.Printf("%s disconnected.", conn.RemoteAddr().String())
+	}
+}
