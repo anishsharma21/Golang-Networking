8ddcac1b01efdcef3db38924232df09dcb8123e9
anishsharma21 <anisharma2103@gmail.com>
2024-12-01 11:40:41 +1100

    Projects - intermediate TCP chat client/server - goroutines for listening to server messages then using a mutex to display the message from another client to the screen with thread safety - this printing functionality is in a separate goroutine and each goroutine is coordinated with a channel - there is a bug currently that needs to be fixed in the following commits where the second message onwards blocks continuously.

--- projects/intermediate_chatserver/client/main.go
+++ projects/intermediate_chatserver/client/main.go
@@ -10,13 +10,16 @@ import (
 	"net"
 	"os"
 	"strings"
+	"sync"
 	"time"
 )
 
-// TODO clear current line when other client messages broadcasted with this: fmt.Print("\r\033[K"), \r to go to start of line, other ANSI escape code to clear the line
-// TODO goroutine listening for messages from server (for broadcast functionality)
+// TODO channels for checking that messages with specific ID's have been responded too
+// TODO custom protocol for receiving messages where there are 2 lines, first for message, second for client remoate address string
+// FIXME bug where second message to server doesn't receive a response
 
 const defaultPort uint16 = 8080
+var printMu sync.Mutex
 
 func main() {
 	conn, err := net.Dial("tcp", fmt.Sprintf("localhost:%d", defaultPort))
@@ -35,6 +38,12 @@ func main() {
 	fmt.Printf("Connected to server on port %d...\n", defaultPort)
 	fmt.Print(">> ")
 
+	responseChan := make(chan string)
+	defer close(responseChan)
+
+	go listenForServerMessages(conn, responseChan)
+	go printMessages(responseChan)
+
 	for scanner.Scan() {
 		message := []byte(scanner.Text())
 		length := uint16(len(message))
@@ -47,8 +56,7 @@ func main() {
 			return
 		}
 
-		_, err = buf.Write(message)
-		if err != nil {
+		if _, err = buf.Write(message); err != nil {
 			log.Printf("Error writing message to buffer: %v\n", err)
 			return
 		}
@@ -58,47 +66,46 @@ func main() {
 			log.Printf("Error sending message to server: %v\n", err)
 			return
 		}
-		messageSentTime := time.Now()
-
-		lengthBuff := make([]byte, 2)
-
-		err = conn.SetReadDeadline(time.Now().Add(5 * time.Second))
-		if err != nil {
-			fmt.Printf("Error setting read deadline: %v\n", err)
-			return
-		}
-		_, err = conn.Read(lengthBuff)
-		if err != nil {
-			if err != io.EOF {
-				log.Printf("Error reading in length of message from server: %v\n", err)
-			} else {
-				log.Println("Server closed.")
-			}
-			return
-		}
-		log.Printf("Response received in: %v\n", time.Since(messageSentTime))
-
-		responseLength := uint16(lengthBuff[0]) << 8 + uint16(lengthBuff[1])
-		responseBuff := make([]byte, responseLength)
-
-		err = conn.SetReadDeadline(time.Now().Add(5 * time.Second))
-		if err != nil {
-			fmt.Printf("Error setting read deadline: %v\n", err)
-			return
-		}
-		_, err = conn.Read(responseBuff)
-		if err != nil {
-			log.Printf("Error receiving response from server: %v\n", err)
-			return
-		}
-
-		fmt.Printf("Server: %v\n", strings.TrimRight(string(responseBuff), "\r\n"))
-		fmt.Print(">> ")
 	}
 
 	if err := scanner.Err(); err != nil {
 		log.Printf("Error reading from input: %v\n", err)
 		return
+	}
+}
+
+func listenForServerMessages(conn net.Conn, serverChannel chan<- string) {
+	lengthBuff := make([]byte, 2)
+	_, err := conn.Read(lengthBuff)
+	if err != nil {
+		if err != io.EOF {
+			log.Printf("Error reading in length of message from server: %v\n", err)
+		} else {
+			log.Println("Server closed.")
+		}
+		return
+	}
+
+	responseLength := uint16(lengthBuff[0]) << 8 + uint16(lengthBuff[1])
+	responseBuff := make([]byte, responseLength)
+
+	_, err = conn.Read(responseBuff)
+	if err != nil {
+		log.Printf("Error receiving response from server: %v\n", err)
+		return
+	}
+
+	serverChannel <- fmt.Sprintf("Server: %v\n", strings.TrimRight(string(responseBuff), "\r\n"))
+}
+
+func printMessages(serverChannel <-chan string) {
+	for {
+		serverMessage := <- serverChannel
+		printMu.Lock()
+		fmt.Print("\r\033[K")
+		fmt.Printf("Client #?: %s\n", serverMessage)
+		fmt.Print(">> ")
+		printMu.Unlock()
 	}
 }
 
