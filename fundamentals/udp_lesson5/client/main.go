ed7f9810c86e1924f2513027d2c5743bdfacad9e
anishsharma21 <anisharma2103@gmail.com>
2024-12-04 00:48:41 +1100

    Fundamentals - UDP network programming - UDP client written to send messages, length of message is relayed back - core functionality to test that makes UDP different is having many stateless packets sent from various clients and seeing if it can concurrently handle each one of them.

---
+++ fundamentals/udp_lesson5/client/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"net"
+	"os"
+)
+
+const DEFAULT_PORT int = 8080
+const DATAGRAM_BUFFER_SIZE = 1024
+
+func main() {
+	conn, err := net.Dial("udp", fmt.Sprintf("localhost:%d", DEFAULT_PORT))
+	if err != nil {
+		fmt.Printf("Error connecting to server on port %d: %v\n", DEFAULT_PORT, err)
+		return
+	}
+	defer conn.Close()
+
+	fmt.Printf("Connected to UDP server on port %d...\n", DEFAULT_PORT)
+
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		message := scanner.Text()
+		_, err := conn.Write([]byte(message))
+		if err != nil {
+			fmt.Printf("Error sending message %q to server %s: %v\n", message, conn.RemoteAddr().String(), err)
+			return
+		}
+
+		buffer := make([]byte, DATAGRAM_BUFFER_SIZE)
+		n, err := conn.Read(buffer)
+		if err != nil {
+			fmt.Printf("Error receiving response from server %s: %v\n", conn.RemoteAddr().String(), err)
+			return
+		}
+		fmt.Printf("Server: %s", buffer[:n])
+	}
+
+	if err := scanner.Err(); err != nil {
+		if err != io.EOF {
+			fmt.Printf("Error scanning input: %v\n", err)
+			return
+		}
+		fmt.Printf("End of file encountered: %v\n", err)
+	}
+}
