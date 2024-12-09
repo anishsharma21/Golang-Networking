dd348e799042848cf9508b0440a374a8cc33c8fc
anishsharma21 <anisharma2103@gmail.com>
2024-12-09 19:23:58 +1100

    Fundamentals - http programming - added a few more handlers, json marshalling too.

--- fundamentals/http_lessons/first_http_lesson/main.go
+++ fundamentals/http_lessons/first_http_lesson/main.go
@@ -1,22 +1,40 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
-	"log"
 	"net/http"
 )
 
 func helloHandler(w http.ResponseWriter, r *http.Request) {
-	log.Println("/ requested")
-	fmt.Fprintf(w, "Hello, world!")
+    fmt.Fprintf(w, "Hello, world!")
+}
+
+func greetHandler(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "Greetings from the server!")
+}
+
+func jsonHandler(w http.ResponseWriter, r *http.Request) {
+    response := map[string]string{"message": "Hello, JSON!"}
+    jsonData, err := json.Marshal(response)
+    if err != nil {
+        http.Error(w, "Error generating JSON", http.StatusInternalServerError)
+        return
+    }
+    w.Header().Set("Content-Type", "application/json")
+    w.Write(jsonData)
 }
 
 func main() {
-	http.HandleFunc("/", helloHandler)
-	fmt.Println("HTTP server starting on port 8080...")
-	err := http.ListenAndServe(":8080", nil)
-	if err != nil {
-		fmt.Printf("Error starting HTTP server on port 8080: %v\n", err)
-		return
-	}
+    http.HandleFunc("/", helloHandler)
+    http.HandleFunc("/greet", greetHandler)
+    http.HandleFunc("/json", jsonHandler)
+    http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
+
+    fmt.Println("HTTP server starting on port 8080...")
+    err := http.ListenAndServe(":8080", nil)
+    if err != nil {
+        fmt.Printf("Error starting HTTP server on port 8080: %v\n", err)
+        return
+    }
 }
