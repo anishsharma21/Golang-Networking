9079f4229a1e841a07707908c406967df5a406e3
anishsharma21 <anisharma2103@gmail.com>
2024-12-13 14:06:37 +1100

    Fundamentals - http exercises - form page where user can enter their name, on submission, get action is triggered and user is sent to the hello page where the url query param is parsed and presented.

--- fundamentals/http_lessons/http_exercises/first_exercise_set.go
+++ fundamentals/http_lessons/http_exercises/first_exercise_set.go
@@ -2,14 +2,18 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"log"
 	"net/http"
 )
+
+var tmpl = template.Must(template.ParseFiles("form.html"))
 
 func main() {
 	http.HandleFunc("/", helloWorldHandler)
 	http.HandleFunc("/hello", helloHandler)
 	http.HandleFunc("/goodbye", goodbyeHandler)
+	http.HandleFunc("/form", formHandler)
 
 	fmt.Println("Starting HTTP server on port 8080...")
 	err := http.ListenAndServe(":8080", nil)
@@ -35,4 +39,14 @@ func helloHandler(w http.ResponseWriter, r *http.Request) {
 
 func goodbyeHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "goodbye!\n")
+}
+
+func formHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodGet {
+		w.Header().Set("Content-Type", "text/html")
+		err := tmpl.Execute(w, nil)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+	}
 }
