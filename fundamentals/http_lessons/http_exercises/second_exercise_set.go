ad39c11e67b112d23cb216749d4f49b0fe120dad
anishsharma21 <anisharma2103@gmail.com>
2024-12-15 13:21:30 +1100

    Fundamentals - http exercises - /divide endpoint, data sent in JSON format, unmarshalled, errors handled using http.Error() builtin function, handling divide by 0 issue, reading in data properly, registered with mux.

--- fundamentals/http_lessons/http_exercises/second_exercise_set.go
+++ fundamentals/http_lessons/http_exercises/second_exercise_set.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io"
 	"log"
 	"net/http"
 )
@@ -22,10 +24,51 @@ func main() {
 		}
 	})
 
+	mux.HandleFunc("GET /data", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Here is some data")
+	})
+
+	mux.HandleFunc("POST /data", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Data saved")
+	})
+
+	mux.HandleFunc("POST /divide", handleDivide)
+
 	log.Println("Starting HTTP server on port 8080...")
 	err := http.ListenAndServe(":8080", mux)
 	if err != nil {
 		log.Printf("Error starting HTTP server on port 8080: %v\n", err)
 		return
+	}
+}
+
+type DivideData struct {
+	X int `json: "x"`
+	Y int `json: "y"`
+}
+
+func handleDivide(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodPost {
+		bodyJson, err := io.ReadAll(r.Body)
+		if err != nil {
+			log.Printf("handleDivide: error reading data from request body: %v\n", err)
+			http.Error(w, "error reading request body", http.StatusInternalServerError)
+			return
+		}
+
+		divideData := new(DivideData)
+		err = json.Unmarshal(bodyJson, divideData)
+		if err != nil {
+			fmt.Printf("handleDivide: error parsing JSON data: %v\n", err)
+		}
+
+		if divideData.Y == 0 {
+			log.Printf("handleDivide: cannot divide by 0\n")
+			http.Error(w, "cannot divide by 0", http.StatusBadRequest)
+			return
+		}
+
+		result := float64(divideData.X) / float64(divideData.Y)
+		fmt.Fprintf(w, "%d / %d = %.4f\n", divideData.X, divideData.Y, result)
 	}
 }
